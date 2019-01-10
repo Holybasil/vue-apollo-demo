53166b514f79320b597d3f65a227b0ca17e4e0ea
xuyunan <youyulan@huli.com>
2019-01-10 11:23:16 +0800

    update readme

--- README.md
+++ README.md
@@ -1,29 +1,30 @@
 # vue-apollo-graphql-pure
 
 ## Project setup
-```
+```bash
+// for client
 npm install
-```
-
-### Compiles and hot-reloads for development
-```
 npm run serve
+
+// for server
+cd server
+npm install
+npm run dev
 ```
 
-### Compiles and minifies for production
-```
-npm run build
-```
+## Iteration
+### 0.0.1
+* client采用在Vue组件中注入apollo选项进行查询
+* server讲type定义在单独的.gql文件中 方便查看与修改
+* 使用mongoose来连接[mongoDB的云服务](https://docs.mlab.com/)
 
-### Run your tests
-```
-npm run test
-```
+### 0.0.2
+...
 
-### Lints and fixes files
-```
-npm run lint
-```
+## TODO
+* [ ] 单表扩为多表 进行多表查询
+* [ ] 确定 client 端最适合使用gql查询的方式
+* [ ] 确定 types 和 resolves 最好的文件结构
+* [ ] ...
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+
--- src/App.vue
+++ src/App.vue
@@ -2,46 +2,46 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-     {{user}}
-
+    {{user}}
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-import gql from 'graphql-tag'
+import gql from "graphql-tag";
 export default {
-  name: 'app',
-  data(){
+  name: "app",
+  data() {
     return {
       id: "1",
-      user: ''
-    }
+      user: ""
+    };
   },
   apollo: {
-  // 简单的查询，将更新 'hello' 这个 vue 属性
-  user: {
-    // gql 查询
-    query: gql`query UserMessage($id: String!) {
-      user(id: $id){
-        name
+    user: {
+      // gql 查询
+      query: gql`
+        query UserMessage($id: String!) {
+          user(id: $id) {
+            name
+          }
+        }
+      `,
+      // 静态参数
+      variables: {
+        id: "1"
       }
-    }`,
-    // 静态参数
-    variables: {
-      id: '1',
-    },
+    }
   },
-},
   components: {
     // HelloWorld
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
